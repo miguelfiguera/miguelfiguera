15b99f18731e5b0f3dc2d2b058b6c79a6ff3aacc
Miguel Figuera <miguelqui725@gmail.com>
2023-05-01 17:16:13 -0400

    Write content for home and header.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { HashLink as Link } from 'react-router-hash-link';
 import uniqid from 'uniqid'
 import bootstrap from 'bootstrap'
+import Home from './Componentes/Home';
+import Header from './Componentes/Header';
+
 
 function App() {
   return (
     <div className="App">
       Personal web Site
+    <Header/>
+
+      <Home />
     </div>
   );
 }
