bdfe98537066bf67fed91a54ef36615a0180637a
Miguel Figuera <miguelqui725@gmail.com>
2023-05-01 17:20:12 -0400

    add navbar content

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import uniqid from 'uniqid'
 import bootstrap from 'bootstrap'
 import Home from './Componentes/Home';
 import Header from './Componentes/Header';
-
+import Navbar from './Componentes/Navbar'
 
 function App() {
   return (
     <div className="App">
       Personal web Site
     <Header/>
-
+    <Navbar/>
       <Home />
     </div>
   );
