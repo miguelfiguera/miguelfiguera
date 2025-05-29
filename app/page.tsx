c5dee87d50ba853a0c4b5fc69fbb219b37984a8e
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2025-05-29 11:04:47 -0400

    change: root page.tsx is now ssr.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Hero from "@/components/landingPage/Hero";
 import RecentProjects from "@/components/landingPage/RecentProjects";
 import CTA from "@/components/GeneralUse/CTA";
