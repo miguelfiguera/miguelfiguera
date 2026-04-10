ec803c075837ff25fbdae144f27ed99421829b70
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2026-04-10 02:36:38 -0400

    feat: add technologies data

---
+++ src/data/technologies.ts
@@ -0,0 +1,52 @@
+export interface Technology {
+  name: string;
+  iconName: string;
+}
+
+export interface TechnologyCategory {
+  category: string;
+  items: Technology[];
+}
+
+export const technologies: TechnologyCategory[] = [
+  {
+    category: "Languages & Frameworks",
+    items: [
+      { name: "React", iconName: "React" },
+      { name: "Next.js", iconName: "Nextjs" },
+      { name: "Rails", iconName: "Rails" },
+      { name: "Laravel", iconName: "Laravel" },
+      { name: "Express", iconName: "Express" },
+      { name: "NestJS", iconName: "NestJS" },
+      { name: "Inertia.js", iconName: "InertiaJS" },
+    ],
+  },
+  {
+    category: "Python",
+    items: [
+      { name: "Scrapy", iconName: "Scrapy" },
+      { name: "Odoo", iconName: "Odoo" },
+    ],
+  },
+  {
+    category: "Databases",
+    items: [
+      { name: "PostgreSQL", iconName: "PostgreSQL" },
+      { name: "MySQL", iconName: "MySQL" },
+      { name: "SQLite", iconName: "SQLite" },
+      { name: "Firebase", iconName: "Firebase" },
+      { name: "MongoDB", iconName: "MongoDB" },
+    ],
+  },
+  {
+    category: "DevOps",
+    items: [
+      { name: "Docker", iconName: "Docker" },
+      { name: "DigitalOcean", iconName: "DigitalOcean" },
+      { name: "Heroku", iconName: "Heroku" },
+      { name: "Netlify", iconName: "Netlify" },
+      { name: "Vercel", iconName: "Vercel" },
+      { name: "Ubuntu", iconName: "Ubuntu" },
+    ],
+  },
+];
